1e70e7174019687ba4c992b0948a0baaac18e4b6
JuanDZM2105 <juandzm2105@gmail.com>
2023-08-31 22:40:39 -0500

    peliculas,documentales y noticias

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from.models import Movie
 
 
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>welcome to home page</h1>')
     #return render(request,'home.html')
-    return render(request,'home.html', {'name':'Juan David Zapata Moncada'})
+    #return render(request,'home.html', {'name':'Juan David Zapata Moncada'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+        
+    return render(request,'home.html',{'movies':movies})
+    
+
+def about(request):
+    #return HttpResponse('<h1>welcome to home page</h1>')
+    #return render(request,'home.html')
+    return render(request,'about.html')
